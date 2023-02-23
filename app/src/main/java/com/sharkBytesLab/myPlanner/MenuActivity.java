c1485dacafaefae2c7dc68c36f3e3b80475ac994
shashankmangal10@gmail.com <Sunil@1234>
2023-02-23 15:51:16 +0530

    privacy and terms left and logo text png

--- app/src/main/java/com/sharkBytesLab/myPlanner/MenuActivity.java
+++ app/src/main/java/com/sharkBytesLab/myPlanner/MenuActivity.java
@@ -2,16 +2,185 @@ package com.sharkBytesLab.myPlanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
-public class MenuActivity extends AppCompatActivity {
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxAdListener;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.ads.MaxAdView;
+import com.applovin.mediation.ads.MaxInterstitialAd;
+import com.sharkBytesLab.myPlanner.databinding.ActivityMenuBinding;
+
+import java.util.concurrent.TimeUnit;
+
+public class MenuActivity extends AppCompatActivity implements MaxAdListener {
+
+    private ActivityMenuBinding binding;
+    private MaxInterstitialAd interstitialAd;
+
+    private MaxAdView adView;
+    private int retryAttempt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().hide();
 
+        binding.applovinAdMenu.loadAd();
+        interstitialAd = new MaxInterstitialAd( "81e3415423b47939", this );
+        interstitialAd.setListener( this );
+        interstitialAd.loadAd();
 
+        binding.supportUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ( interstitialAd.isReady() )
+                {
+                    interstitialAd.showAd();
+                }
+
+            }
+        });
+
+        binding.homeButtonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        binding.share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    String shareMsg = "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n\n";
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "Camera Studio : Free-Image-Editor");
+                    intent.putExtra(Intent.EXTRA_TEXT, shareMsg);
+                    startActivity(Intent.createChooser(intent, "Share Via"));
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Error Occured :" + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.rateUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName());
+                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+
+                try {
+                    startActivity(i);
+                } catch (Exception e) {
+
+                    Toast.makeText(getApplicationContext(), "Error :" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        binding.update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName());
+                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+
+                try {
+                    startActivity(i);
+                } catch (Exception e) {
+
+                    Toast.makeText(getApplicationContext(), "Error :" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuActivity.this, PrivacyPolicyActivity.class));
+                finish();
+            }
+        });
+
+        binding.terms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuActivity.this, TermsActivity.class));
+                finish();
+            }
+        });
+
+        binding.version.setText("Version : " + String.valueOf(BuildConfig.VERSION_NAME));
+
+    }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(MenuActivity.this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onAdLoaded(final MaxAd maxAd)
+    {
+        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
+        // Reset retry attempt
+        retryAttempt = 0;
+    }
+
+    @Override
+    public void onAdLoadFailed(final String adUnitId, final MaxError error)
+    {
+        // Interstitial ad failed to load
+        // AppLovin recommends that you retry with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+
+        retryAttempt++;
+        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                interstitialAd.loadAd();
+            }
+        }, delayMillis );
+    }
+
+    @Override
+    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
+    {
+        // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
+        interstitialAd.loadAd();
+    }
+
+    @Override
+    public void onAdDisplayed(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdClicked(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdHidden(final MaxAd maxAd)
+    {
+        // Interstitial ad is hidden. Pre-load the next ad
+        interstitialAd.loadAd();
     }
 }
