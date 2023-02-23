78115819dcd3547e45a28d29495cbffea6f2112d
shashankmangal10@gmail.com <Sunil@1234>
2023-02-23 16:07:49 +0530

    txt logo left

--- app/src/main/java/com/sharkBytesLab/myPlanner/PrivacyPolicyActivity.java
+++ app/src/main/java/com/sharkBytesLab/myPlanner/PrivacyPolicyActivity.java
@@ -2,13 +2,38 @@ package com.sharkBytesLab.myPlanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.sharkBytesLab.myPlanner.databinding.ActivityPrivacyPolicyBinding;
+
 public class PrivacyPolicyActivity extends AppCompatActivity {
+
+    private ActivityPrivacyPolicyBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_privacy_policy);
+        binding = ActivityPrivacyPolicyBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        try {
+
+            binding.policyWebView.getSettings().setJavaScriptEnabled(true);
+            binding.policyWebView.loadUrl("file:///android_asset/policy.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(PrivacyPolicyActivity.this, MenuActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/sharkBytesLab/myPlanner/TermsActivity.java
+++ app/src/main/java/com/sharkBytesLab/myPlanner/TermsActivity.java
@@ -2,13 +2,37 @@ package com.sharkBytesLab.myPlanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.sharkBytesLab.myPlanner.databinding.ActivityTermsBinding;
 
 public class TermsActivity extends AppCompatActivity {
 
+    private ActivityTermsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_terms);
+        binding = ActivityTermsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        getSupportActionBar().hide();
+
+        try {
+
+            binding.termsWebView.getSettings().setJavaScriptEnabled(true);
+            binding.termsWebView.loadUrl("file:///android_asset/terms.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(TermsActivity.this, MenuActivity.class));
+        finish();
     }
 }
